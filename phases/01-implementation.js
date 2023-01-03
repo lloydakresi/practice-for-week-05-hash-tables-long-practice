40275be152b969daa8006525c4d1b16ad298b895
Lloyd Akresi <lloydakresi03@gmail.com>
2023-01-03 19:21:17 +0000

    complete

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -11,6 +11,9 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
   constructor(numBuckets = 8) {
     // Initialize your buckets here
     // Your code here
+    this.data = new Array(numBuckets).fill(null);
+    this.count = 0;
+    this.capacity = numBuckets;
   }
 
   hash(key) {
@@ -31,21 +34,88 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   insert(key, value) {
     // Your code here
+    if((this.count / this.capacity) >= 0.7){
+      this.resize();
+    }
+    let kVPair = new KeyValuePair(key, value);
+    let bucketKey = this.hashMod(key);
+
+    if(this.data[bucketKey]){
+      let bucketPair = this.data[bucketKey];
+
+      while(bucketPair !== null && bucketPair.key !== key){
+        bucketPair = bucketPair.next;
+      }
+
+      if (bucketPair === null){
+        let prev = this.data[bucketKey];
+        this.data[bucketKey] = kVPair;
+        this.data[bucketKey].next = prev;
+        this.count++;
+      }else{
+        bucketPair.value = kVPair.value;
+      }
+
+    }else{
+      this.data[bucketKey] = kVPair;
+      this.count++;
+
+    }
   }
 
 
   read(key) {
     // Your code here
+    let mod = this.hashMod(key);
+    let bucketPair = this.data[mod];
+
+      while(bucketPair !== null && bucketPair.key !== key){
+        bucketPair = bucketPair.next;
+      }
+      if(bucketPair === null) return undefined ;
+      return bucketPair.value;
+
   }
 
 
   resize() {
     // Your code here
+    this.capacity*=2;   //double the current capacity
+    let oldArr = this.data.slice();   //create a copy of the current this.data
+    this.data = new Array(this.capacity).fill(null); //create a new array with double the new capacity
+    let countCopy = this.count; // make a copy of the current count
+
+    for(let i = 0; i < oldArr.length; i++){   //assigning the values
+      let ele = oldArr[i];
+      while(ele){ //while ele !== null
+        this.insert(ele.key, ele.value); // insert the new key and value into the new array
+        ele = ele.next; // traverse the bucket if the data is a linked list
+      }
+    }
+    this.count = countCopy; // assign the count var to this.copy
   }
 
 
   delete(key) {
     // Your code here
+    let mod = this.hashMod(key);
+    let delKey = this.data[mod];
+    let prev;
+
+    while(delKey !== null && (delKey.key !== key)){
+      prev = delKey;
+      delKey = delKey.next;
+    }
+
+    if (delKey === null) return "Key not found";
+
+    if (prev){
+      prev.next = delKey.next;
+    }else{
+      this.data[mod] = delKey.next;
+    }
+    this.count--;
+
   }
 }
 
--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,26 +1,95 @@
 function anagrams(str1, str2) {
   // Your code here
+  let table1 = table(str1);
+  let table2 = table(str2);
+
+  for(let letter of str1){
+    if(table1[letter] !== table2[letter]) return false;
+  }
+
+  for(let letter of str2){
+    if(table1[letter] !== table2[letter]) return false;
+  }
+
+  return true;
+}
+
+function table(str){
+  let table = {};
+  for(let i = 0; i < str.length; i++){
+    let letter = str[i];
+    if (table[letter] === undefined) table[letter] = 1;
+    table[letter]++;
+  }
+
+  return table;
 }
 
 
 function commonElements(arr1, arr2) {
   // Your code here
+  let set1 = new Set(arr1);
+  let set2 = new Set(arr2);
+  let set3 = [];
+
+  for(n of set1){
+    if (set2.has(n)) set3.push(n);
+  }
+
+  return set3;
 }
 
 
 function duplicate(arr) {
   // Your code here
+  let set2 = new Set()
+  for(ele of arr){
+    if (!set2.has(ele)){
+      set2.add(ele);
+    }else{
+      return ele;
+    }
+  }
 }
 
 
 function twoSum(nums, target) {
   // Your code here
+  let set = new Set();
+  for(let i = 0; i < nums.length; i++){
+    if(set.has(target - nums[i])) return true;
+    else set.add(nums[i]);
+  }
+  return false;
 }
 
 
 function wordPattern(pattern, strings) {
   // Your code here
+  const model = {};
+  const assigned = new Set()
+
+  for (let i = 0; i < strings.length; i++) {
+    let str = strings[i];
+    let key = pattern[i];
+
+    if ( !model[key] && !assigned.has(str) ) {
+      model[key] = str;
+      assigned.add(str);
+    } else if (model[key] !== str) {
+      return false;
+    }
+  }
+
+  return true;
+
 }
+
+console.log(wordPattern("ABBAA", ['dog', 'cat', 'cat', 'dog', `cat`]))
+console.log(wordPattern("ABBA", ['dog', 'dog', 'dog', 'dog']))
+console.log(wordPattern("AAAA", ['dog', 'dog', 'dog', 'dog']))
+console.log(wordPattern("ABCD", ['dog', 'cat', 'dog', 'cat']))
+
 
 
 module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
